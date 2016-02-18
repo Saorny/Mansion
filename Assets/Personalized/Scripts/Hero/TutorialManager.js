62a037d62f761333226f44f9ff100f8b2aaa8fe0
Saorny <remibartel@hotmail.fr>
2016-02-18 22:11:33 +0800

    Continuing tutorial tools
    Now loads all texts from files.

--- Assets/Personalized/Scripts/Hero/TutorialManager.js
+++ Assets/Personalized/Scripts/Hero/TutorialManager.js
@@ -6,33 +6,33 @@ public class TutorialManager extends MonoBehaviour {
 	private var		_hasDisplayed : Dictionary.<TutorialManager.TutoList, boolean>;
 	private var		_tutorials : Dictionary.<TutorialManager.TutoList, String>;
 	private var		_tutoDuration : float = 10.0;
+	private var		COMMON_PATH : String = 'Texts/Tutorials/';
 	
 	public enum	TutoList { 	ACCESS_MENU, ACCESS_INVENTORY, SANITY_WARNING,
 							COLLECT_OBJECT, OBJECT_COLLECTED, BEING_HIT, USE_POTION }
 	
 	public function Awake() : void {
-	
 		var menuItems = new Array();
+		var	res : TextAsset;
 		
 		this._hasDisplayed = new Dictionary.<TutorialManager.TutoList, boolean>();
 		this._tutorials = new Dictionary.<TutorialManager.TutoList, String>();
 		
 		menuItems = TutoList.GetValues(typeof(TutoList));
-		for (var i = 0 ; i < menuItems.Count ; ++i)
+		for (var i = 0 ; i < menuItems.Count ; ++i) {
 			this._hasDisplayed[menuItems[i]] = false;
-		this.loadTutos();
+			Debug.Log(menuItems[i].ToString());
+			res = Resources.Load(this.COMMON_PATH + menuItems[i].ToString(), typeof(TextAsset));
+			if (res != null) {
+				this._tutorials[menuItems[i]] = res.ToString();
+			}
+		}
 	}
 	
 	public function mayDislayTuto(type : TutoList, displayPtr : function(String, float, Message.messageType) : void) : void {
-	
 		if (this._hasDisplayed[type] == false) {
 			this._hasDisplayed[type] = true;
 			displayPtr(this._tutorials[type] as String, this._tutoDuration, Message.messageType.TUTORIAL);
 		}
-	}
-	
-	private function loadTutos() : void {
-		this._tutorials[TutoList.ACCESS_MENU] = 'You may press "Escape" to access the menu. Do not hesitate the read the tutorial!';
-		this._tutorials[TutoList.ACCESS_INVENTORY] = 'You may press "i" to access your inventory.';
 	}
 }
